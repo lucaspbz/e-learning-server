8f6653479e72f98f3e998bcf46d54103c9620ccd
Lucas Pinheiro <lucas.pbezerra@hotmail.com>
2020-11-05 13:53:00 -0300

    ensureAuthenticated middleware created user needs to be authenticated now to create and update a course

--- src/modules/courses/infra/http/routes/courses.routes.ts
+++ src/modules/courses/infra/http/routes/courses.routes.ts
@@ -1,33 +1,42 @@
 import CreateCourseService from '@modules/courses/services/CreateCourseService';
 import UpdateCourseService from '@modules/courses/services/UpdateCourseService';
+import ensureAuthenticated from '@shared/infra/http/middlewares/ensureAuthenticated';
 import { Request, Response, Router } from 'express';
 import { container } from 'tsyringe';
 
 const coursesRouter = Router();
 
-coursesRouter.post('/', async (request: Request, response: Response) => {
-  const { name, image } = request.body;
+coursesRouter.post(
+  '/',
+  ensureAuthenticated,
+  async (request: Request, response: Response) => {
+    const { name, image } = request.body;
 
-  const createCourse = container.resolve(CreateCourseService);
+    const createCourse = container.resolve(CreateCourseService);
 
-  const course = await createCourse.execute({ name, image });
+    const course = await createCourse.execute({ name, image });
 
-  return response.status(201).json(course);
-});
+    return response.status(201).json(course);
+  },
+);
 
-coursesRouter.put('/:id', async (request: Request, response: Response) => {
-  const { id } = request.params;
-  const { name, image } = request.body;
+coursesRouter.put(
+  '/:id',
+  ensureAuthenticated,
+  async (request: Request, response: Response) => {
+    const { id } = request.params;
+    const { name, image } = request.body;
 
-  const updateCourse = container.resolve(UpdateCourseService);
+    const updateCourse = container.resolve(UpdateCourseService);
 
-  const course = await updateCourse.execute({
-    course_id: id,
-    name,
-    image,
-  });
+    const course = await updateCourse.execute({
+      course_id: id,
+      name,
+      image,
+    });
 
-  return response.json(course);
-});
+    return response.json(course);
+  },
+);
 
 export default coursesRouter;
